2cf13a3d7e72717d97c6d23d0f6def69884952a8
PULKIT <mukeshpulkit76@gmail.com>
2022-06-28 13:19:36 +0530

    Day1

---
+++ countdown-timer/script.js
@@ -0,0 +1,23 @@
+const newYears = '1 Jan 2023';
+ 
+function countdown(){
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const seconds = (newYearsDate - currentDate) /1000;
+
+
+    const days = Math.floor(seconds / 3600 / 24);
+    const hours = Math.floor(seconds / 3600)% 24;
+    const minutes = Math.floor(seconds/ 60)%60;
+    
+
+    console.log(days, hours, minutes);
+}
+
+
+
+countdown();
+
+
+
