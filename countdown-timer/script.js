b2ce223b1d49ea1b64cbc4c3ac2ef226b9a135e1
PULKIT <mukeshpulkit76@gmail.com>
2022-06-29 13:34:44 +0530

    Project1 Complete

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -1,23 +1,41 @@
+const daysEl =  document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+
+
+
 const newYears = '1 Jan 2023';
  
 function countdown(){
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
 
-    const seconds = (newYearsDate - currentDate) /1000;
+    const totalSeconds = (newYearsDate - currentDate) /1000;
 
 
-    const days = Math.floor(seconds / 3600 / 24);
-    const hours = Math.floor(seconds / 3600)% 24;
-    const minutes = Math.floor(seconds/ 60)%60;
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600)% 24;
+    const mins = Math.floor(totalSeconds/ 60)%60;
+    const seconds = Math.floor(totalSeconds)%60;
+
+
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+
+
     
-
-    console.log(days, hours, minutes);
 }
-
+function formatTime(time) {
+    return time<10 ? (`0${time}`) :time;
+}
 
 
 countdown();
 
 
-
+setInterval(countdown, 1000);
